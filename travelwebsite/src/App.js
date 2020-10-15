229f21abc04af2d9f9c95a8ef09560dbe9e95c7d
Asu Kaur <asukaur99@gmail.com>
2020-10-16 00:49:18 +0530

    Template Done

--- travelwebsite/src/App.js
+++ travelwebsite/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Components/Navbar';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Components/pages/Home";
+import Services from "./Components/pages/Services";
+import Products from "./Components/pages/Products";
+import SignUp from "./Components/pages/SignUp";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
-    <Router><Navbar></Navbar>
-    <Switch>
-      <Route path='/' exact>
-        </Route></Switch></Router>
-      
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/services" exact component={Services}></Route>
+          <Route path="/product" exact component={Products}></Route>
+          <Route path="/sign-up" exact component={SignUp}></Route>
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }
---
+++ travelwebsite/src/Components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.css";
+import Card from "../Card";
+import Hero from "../Hero";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Card />
+    </>
+  );
+}
+
+export default Home;
